e7a88f2ce7745eda7290225f4289418f8ea369b9
Bernardo D'Auria <brdauria@gmail.com>
2019-03-25 13:51:06 +0100

    Update build_one.R

--- R/build_one.R
+++ R/build_one.R
@@ -6,24 +6,21 @@ local({
   markdown = blogdown:::get_config2('markdown', 'kramdown')
   # see if we need to use the Jekyll renderer in knitr
   if (markdown == 'kramdown') knitr::render_jekyll() else knitr::render_markdown()
-
+  
   # input/output filenames are passed as two additional arguments to Rscript
   a = commandArgs(TRUE)
-  d = gsub('^_|[.][a-zA-Z]+$', '', a[1])
+  # d = gsub('^_|[.][a-zA-Z]+$', '', a[1])
+  d = gsub('^([._/]+)|[.][a-zA-Z]+$', '', a[1])
   knitr::opts_chunk$set(
-    fig.path   = sprintf('figure/%s/', d),
+    # fig.path   = sprintf('assets/figures/%s/', d),
+    fig.path = sprintf('%s/', gsub('^.+/', '', d)),
     cache.path = sprintf('cache/%s/', d)
   )
-  # set where you want to host the figures (I store them in my Dropbox folder
-  # served via Updog.co, but you might prefer putting them in GIT)
-  if (Sys.getenv('USER') == 'yihui') {
-    # these settings are only for myself, and they will not apply to you, but
-    # you may want to adapt them to your own website
-    knitr::opts_chunk$set(fig.path = sprintf('%s/', gsub('^.+/', '', d)))
-    knitr::opts_knit$set(
-      base.dir = '~/Dropbox/Apps/updog/drop/jekyll/',
-      base.url = 'https://db.yihui.name/jekyll/'
-    )
+  if (getOption('blogdown.base.dir', '') != '') {
+    knitr::opts_knit$set(base.dir = getOption('blogdown.base.dir', ''))
+  }
+  if (getOption('blogdown.base.url', '') != '') {
+    knitr::opts_knit$set(base.url = getOption('blogdown.base.url', ''))
   }
   options(digits = 4)
   knitr::opts_knit$set(width = 70)
